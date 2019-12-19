a40cb8ac6d3279458660b83576454a4f3dd4f1dd
hankhu <hankhu@webim.com.tw>
2019-12-19 17:23:37 +0800

    sw

---
+++ precache-manifest.98018feb354c21728fb3f670deb5e581.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac174e5cb58f3b9489e",
+    "url": "/css/app.6ca6236a.css"
+  },
+  {
+    "revision": "7a0acb9b363ff277c199",
+    "url": "/css/chunk-vendors.d171c6d2.css"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fonts/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fonts/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fonts/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fonts/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "8977bba93d5cba715951475f6ab7f274",
+    "url": "/formio.full.min.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/img/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "78a7b0bc4c1e4827342f23a3c1caadba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac174e5cb58f3b9489e",
+    "url": "/js/app.60da1681.js"
+  },
+  {
+    "revision": "7a0acb9b363ff277c199",
+    "url": "/js/chunk-vendors.aace8330.js"
+  },
+  {
+    "revision": "017ced36d82bea1e08b08393361e354d",
+    "url": "/js/db/p_idb.js"
+  },
+  {
+    "revision": "e76ea70e6d27c104ba1bd4a887037034",
+    "url": "/js/db/p_indexedDB.js"
+  },
+  {
+    "revision": "c2a05c0cd323c9a0a4658539db207047",
+    "url": "/js/swActions.js"
+  },
+  {
+    "revision": "640f0a12c23ae5e90cdb3ec3ae88ae86",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
